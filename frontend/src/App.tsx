1233c6ff2bd79a7752bf23189a3c57a4d704ff1a
misat1505 <misat2002@gmail.com>
2023-11-30 11:10:33 +0100

    set up basic route with suspense

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { lazy, Suspense, ComponentType, CSSProperties } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const Home = lazy(() => import("./pages/Home"));
 
 function App() {
+  const suspensify = (Component: ComponentType) => {
+    const suspenseStyles: CSSProperties = {
+      width: "100%",
+      height: "100vh",
+      backgroundColor: "var(--primary-color)",
+    };
+
+    return (
+      <Suspense
+        fallback={
+          <div style={suspenseStyles}>
+            {/* <Loading text="" /> */}
+            loading...
+          </div>
+        }
+      >
+        <Component />
+      </Suspense>
+    );
+  };
+
+  const routes = [
+    {
+      path: "/",
+      component: Home,
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        {routes.map((route, index) => (
+          <Route
+            key={index}
+            path={route.path}
+            element={suspensify(route.component)}
+          />
+        ))}
+      </Routes>
+    </Router>
   );
 }
 
