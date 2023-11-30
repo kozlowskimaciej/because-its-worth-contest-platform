b04ca3ff4857e9194c4b61e3d610a2e31d338579
kubao503 <59119141+kubao503@users.noreply.github.com>
2023-11-30 22:51:23 +0100

    PZSP2-30: Mongodb fixture (#2)

---
+++ backend/tests/db_integration/test_db_integration.py
@@ -0,0 +1,45 @@
+from testcontainers.mongodb import MongoDbContainer
+from pymongo.database import Database
+from pymongo import MongoClient
+import pytest
+
+
+@pytest.fixture(scope="session")
+def mongo_client() -> MongoClient:
+    with MongoDbContainer() as mongo:
+        yield mongo.get_connection_client()
+
+
+@pytest.fixture
+def shared_db(mongo_client) -> Database:
+    return mongo_client.shared_instance
+
+
+@pytest.fixture
+def unique_db(request: pytest.FixtureRequest, mongo_client) -> Database:
+    return getattr(mongo_client, request.node.name)
+
+
+def test_mongo_insert_and_find(unique_db: Database):
+    doc = {
+        "address": {
+            "street": "2 Avenue",
+            "zipcode": "10075",
+            "building": "1480",
+            "coord": [-73.9557413, 40.7720266],
+        },
+        "borough": "Manhattan",
+        "cuisine": "Italian",
+        "name": "Vella",
+        "restaurant_id": "41704620",
+    }
+    unique_db.restaurants.insert_one(doc)
+    cursor = unique_db.restaurants.find({"borough": "Manhattan"})
+    assert cursor.next()["restaurant_id"] == doc["restaurant_id"]
+
+
+# This test uses other db instance so no data will be found
+def test_mongo_no_data(unique_db: Database):
+    cursor = unique_db.restaurants.find({"borough": "Manhattan"})
+    with pytest.raises(StopIteration):
+        cursor.next()["restaurant_id"]
