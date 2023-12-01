83c5faa7d8a7d16d694cf143f1a950b4bb9a59d5
Adam <a.gepard007@gmail.com>
2023-12-01 13:16:59 +0100

    Included router

--- backend/api/app.py
+++ backend/api/app.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
-
-from api.routers import hello
+from api.routers import hello, static_files
 
 
 def create_app():
@@ -8,6 +7,10 @@ def create_app():
 
     # including routers
     app.include_router(hello.router)
+    app.include_router(static_files.router)
+
+    # static files are available under /uploads path
+    app.mount("/uploads", static_files.static_router, name="uploads")
 
     return app
 
