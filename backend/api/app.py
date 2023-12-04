9b6d397d1fdbbea5f1d940e1595b1da324c16274
AdamJ77 <61241656+AdamJ77@users.noreply.github.com>
2023-12-04 16:43:29 +0100

    Configure fast api router for static files with validation and allowed uploading (#4)

--- backend/api/app.py
+++ backend/api/app.py
@@ -1,13 +1,18 @@
 from fastapi import FastAPI
+# from api.routers import hello, static_files # for development
+from backend.api.routers import hello, static_files # for testing
 
-from api.routers import hello
-
+STATIC_FILES_PATH = "/static_files"
 
 def create_app():
     app = FastAPI()
 
     # including routers
     app.include_router(hello.router)
+    app.include_router(static_files.router)
+
+    # static files are available under /static_files path
+    app.mount(STATIC_FILES_PATH, static_files.static_router, name="uploading")
 
     return app
 
