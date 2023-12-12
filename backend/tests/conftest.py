58a545ee8065c66b5a2f6798b56b8d986f89d48d
Kozprog <maciejmaciejkoz@gmail.com>
2023-12-12 19:11:56 +0100

    Add client fixture

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,7 +1,7 @@
 from pathlib import Path
+from fastapi.testclient import TestClient
 
 from testcontainers.mongodb import MongoDbContainer
-from pymongo.database import Database
 import pytest
 
 
@@ -12,7 +12,7 @@ def mongodb_container():
 
 
 @pytest.fixture
-def db(request, mongodb_container) -> Database:
+def db(request, mongodb_container):
     client = mongodb_container.get_connection_client()
     return getattr(client, request.node.name)
 
@@ -22,3 +22,19 @@ def testdir(monkeypatch, tmpdir) -> Path:
     print(tmpdir)
     monkeypatch.chdir(tmpdir)
     return tmpdir
+
+
+@pytest.fixture
+def client(request, monkeypatch, mongodb_container) -> TestClient:
+    from backend.api.app import create_app, Database
+
+    monkeypatch.setenv(
+        "DB_CONNECTION_URL", mongodb_container.get_connection_url()
+    )
+    monkeypatch.setattr(
+        Database, "get_instance_name", lambda _: request.node.name
+    )
+
+    app = create_app()
+    with TestClient(app) as testclient:
+        yield testclient
