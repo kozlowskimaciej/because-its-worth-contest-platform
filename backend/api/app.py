ef786ac631b85f4222ba75b1f955319e54bf4548
Maciej Kozłowski <maciejmaciejkoz@gmail.com>
2023-12-12 20:48:46 +0100

    PZSP2-11 | Add DB connector (#8)

--- backend/api/app.py
+++ backend/api/app.py
@@ -1,20 +1,33 @@
+import os
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
-# from api.routers import hello, static_files # for development
-from backend.api.routers import hello, static_files  # for testing
+from backend.api.routers.static_files import STATIC_FOLDER_NAME
+from backend.api.database.db import Database
+from backend.api.routers import hello, static_files
 
-STATIC_FILES_PATH = "/static_files"
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    db_client = Database()
+    app.database = await db_client.database
+    yield
+    await db_client.close()
 
 
 def create_app():
-    app = FastAPI()
+    app = FastAPI(lifespan=lifespan)
 
     # including routers
     app.include_router(hello.router)
     app.include_router(static_files.router)
 
     # static files are available under /static_files path
-    app.mount(STATIC_FILES_PATH, static_files.static_router, name="uploading")
+    os.makedirs(STATIC_FOLDER_NAME, exist_ok=True)
+    app.mount(
+        "/static", StaticFiles(directory=STATIC_FOLDER_NAME), name="uploading")
 
     return app
 
---
+++ backend/api/database/db.py
@@ -0,0 +1,31 @@
+import logging
+import os
+
+from motor.motor_asyncio import AsyncIOMotorClient
+
+
+log = logging.getLogger(__file__)
+
+
+class Database:
+    def __init__(self, connection_url: str = None):
+        connection_url = connection_url or os.getenv('DB_CONNECTION_URL')
+        assert connection_url, 'Connection URL to DB not set'
+
+        self.__client = AsyncIOMotorClient(connection_url)
+        self.__database = None
+        log.info(f'Connected to DB\n{self.__client.topology_description}')
+
+    def get_instance_name(self):
+        return "because-its-worth"
+
+    @property
+    async def database(self):
+        if not self.__database:
+            self.__database = self.__client.get_database(
+                self.get_instance_name())
+        return self.__database
+
+    async def close(self):
+        if self.__client:
+            self.__client.close()
