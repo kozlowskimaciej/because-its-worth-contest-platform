70757faab74a413a532997bdf90187850e161874
Maciej Kozłowski <maciejmaciejkoz@gmail.com>
2023-12-17 00:49:00 +0100

    Wrap lines

--- backend/api/routers/contests.py
+++ backend/api/routers/contests.py
@@ -39,7 +39,10 @@ async def post_contest(
     return {'id': str(inserted_id)}
 
 
-@router.get('/', responses={404: {'description': 'Contest with given id not found'}})
+@router.get(
+    '/', 
+    responses={404: {'description': 'Contest with given id not found'}}
+)
 async def get_contests(
     request: Request,
     id: Optional[str] = None,
