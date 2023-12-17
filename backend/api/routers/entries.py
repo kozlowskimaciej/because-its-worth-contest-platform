508ec6f28b839838acc034481b20fdfb1ebf1ca9
AdamJ77 <61241656+AdamJ77@users.noreply.github.com>
2023-12-17 13:23:03 +0100

    PZSP2-10: add entries endpoint (#10)

---
+++ backend/api/routers/entries.py
@@ -0,0 +1,69 @@
+import json
+from typing import Optional
+
+from bson import ObjectId, json_util
+from fastapi import APIRouter, HTTPException
+from pydantic import AnyHttpUrl, BaseModel
+from starlette.requests import Request
+
+MAX_ENTRIES_NUM = 100
+
+router = APIRouter(
+    prefix='/entries',
+    tags=['Entries']
+)
+
+
+class Entry(BaseModel):
+    firstName: str
+    lastName: str
+    guardianFirstName: str
+    guardianLastName: str
+    phone: str
+    email: str
+    address: str
+    submissionDate: str
+    attachments: list[AnyHttpUrl]
+    place: str
+    contestId: str
+
+
+@router.get('/{contestId}')
+async def get_entries(
+    request: Request,
+    contestId: str,
+    entryId: Optional[str] = None
+):
+    db = request.app.database
+    if entryId:
+        entry_id_obj = ObjectId(entryId)
+        entry = await db.entries.find_one(
+            {'_id': entry_id_obj, 'contestId': contestId}
+        )
+
+        if entry:
+            return {'data': json.loads(json_util.dumps(entry))}
+        else:
+            raise HTTPException(
+                status_code=404,
+                detail=f"Entry {entryId} not found"
+            )
+    else:
+        entries = await db.entries.find({'contestId': contestId}) \
+            .to_list(length=MAX_ENTRIES_NUM)
+
+        if not entries:
+            raise HTTPException(
+                status_code=404,
+                detail=f"No entries found for contest id {contestId}"
+            )
+
+        return {'data': json.loads(json_util.dumps(entries))}
+
+
+@router.post('/')
+async def create_entry(entry: Entry, request: Request):
+    db = request.app.database
+    entry_dict = entry.model_dump(mode='json')
+    inserted_id = (await db.entries.insert_one(entry_dict)).inserted_id
+    return {'id': str(inserted_id)}
