3effcf98ab5b80f14fdaa25d68ff88e282542efc
Jakub Stacherski <o503@interia.eu>
2023-12-17 20:40:14 +0100

    Even more assertions

--- backend/tests/emails/test_emails.py
+++ backend/tests/emails/test_emails.py
@@ -32,6 +32,13 @@ def mock_smtp(monkeypatch: MonkeyPatch):
 
 
 def test_send_email(mock_smtp):
+    from email.message import EmailMessage
+
     assert len(mock_smtp) == 0
     email_sending.send_email("maciej@kozlowski.pb.bi", "Henlo", "Email body")
+
     assert len(mock_smtp) == 1
+    msg: EmailMessage = mock_smtp[0]
+    assert msg.get_content().strip() == "Email body"
+    assert msg["Subject"] == "Henlo"
+    assert msg["To"] == "maciej@kozlowski.pb.bi"
