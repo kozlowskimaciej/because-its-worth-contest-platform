b9c56f8002f3989e919eb2df39846760cc8ed220
misat1505 <misat2002@gmail.com>
2023-12-18 22:05:55 +0100

    removed big comment

--- frontend/src/components/rate/Entries.tsx
+++ frontend/src/components/rate/Entries.tsx
@@ -1,55 +1,9 @@
-import React, { useState } from "react";
-import { Entry, Entry as EntryType, ExpandableEntry } from "../../models/Entry";
+import React from "react";
 import EntryGroup from "./EntryGroup";
 import { useRateContext } from "../../contexts/RateContext";
 
 export default function Entries() {
   const { expandableEntries } = useRateContext();
-  // const [expandableEntries, setExpandableEntries] = useState<ExpandableEntry[]>(
-  //   entries.map((entry) => ({
-  //     entry,
-  //     isExpanded: false,
-  //   }))
-  // );
-
-  // const open = (id: string) => {
-  //   setExpandableEntries((prev) => {
-  //     return prev.map((expEntry) => {
-  //       if (expEntry.entry.id !== id) return expEntry;
-
-  //       return {
-  //         isExpanded: true,
-  //         entry: expEntry.entry,
-  //       };
-  //     });
-  //   });
-  // };
-
-  // const close = (id: string) => {
-  //   setExpandableEntries((prev) => {
-  //     return prev.map((expEntry) => {
-  //       if (expEntry.entry.id !== id) return expEntry;
-
-  //       return {
-  //         isExpanded: false,
-  //         entry: expEntry.entry,
-  //       };
-  //     });
-  //   });
-  // };
-
-  // const changePlace = (id: string, newPlace: string | null) => {
-  //   setExpandableEntries((prev) => {
-  //     return prev.map((expEntry) => {
-  //       if (expEntry.entry.id !== id) return expEntry;
-
-  //       return {
-  //         isExpanded: expEntry.isExpanded,
-  //         entry: { ...expEntry.entry, place: newPlace } as Entry,
-  //       };
-  //     });
-  //   });
-  // };
 
   const laureatEntries = expandableEntries.filter(
     (expEntry) => expEntry.entry.place === "laureat"
