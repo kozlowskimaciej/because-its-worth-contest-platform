6d679c4bbf198574a575e39af4b12eb63df474ef
misat1505 <misat2002@gmail.com>
2023-12-25 22:48:06 +0100

    logout for frontend

--- frontend/src/components/common/Navbar.tsx
+++ frontend/src/components/common/Navbar.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "./styles/Navbar.module.css";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { errorConfig, successConfig } from "../../config/toasts";
 
 export default function Navbar() {
   const navigate = useNavigate();
@@ -23,17 +26,46 @@ export default function Navbar() {
     },
   ];
 
+  const handleLogout = () => {
+    if (!window.confirm("Czy na pewno chcesz się wylogować?")) return;
+
+    const id = toast.loading("Proszę czekać...");
+    axios
+      .post(
+        `${process.env.REACT_APP_SERVER_URL}/auth/logout`,
+        {},
+        {
+          withCredentials: true,
+        }
+      )
+      .then((data) => {
+        if (data.status !== 200) throw new Error();
+        toast.update(id, successConfig("Wylogowanie powiodło się."));
+        navigate("/login");
+      })
+      .catch((err) => {
+        toast.update(id, errorConfig("Wystąpił błąd podczas wylogowywania."));
+      });
+  };
+
   return (
     <header className={styles.header}>
-      {buttons.map((button, index) => (
-        <button
-          key={index}
-          onClick={button.isActive ? () => {} : button.onclick}
-          className={`${styles.button} ${button.isActive ? styles.active : ""}`}
-        >
-          {button.text}
-        </button>
-      ))}
+      <div className={styles.buttons}>
+        {buttons.map((button, index) => (
+          <button
+            key={index}
+            onClick={button.isActive ? () => {} : button.onclick}
+            className={`${styles.button} ${
+              button.isActive ? styles.active : ""
+            }`}
+          >
+            {button.text}
+          </button>
+        ))}
+      </div>
+      <button className={styles.logout} onClick={handleLogout}>
+        WYLOGUJ SIĘ
+      </button>
     </header>
   );
 }
