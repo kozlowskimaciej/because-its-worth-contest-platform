184c6e38b2354053bf1f98039fb401a8f98bee18
misat1505 <misat2002@gmail.com>
2023-12-28 23:32:02 +0100

    fixtures in parametres

--- backend/tests/api/test_auth.py
+++ backend/tests/api/test_auth.py
@@ -31,13 +31,13 @@ def setup_users():
     return correct_user, auth_header
 
 
-def test_login(client):
+def test_login(client, setup_users):
     incorrect_user = {
         "login": "incorrect_login",
         "password": "incorrect_password"
     }
 
-    correct_user, _ = setup_users()
+    correct_user, _ = setup_users
 
     response = client.post("/auth/login", json=incorrect_user)
     assert response.status_code == 401
@@ -50,16 +50,16 @@ def test_login(client):
     assert len(ip_token_storage) == 1
 
 
-def test_refresh(client):
-    _, auth_header = setup_users()
+def test_refresh(client, setup_users):
+    _, auth_header = setup_users
 
     response = client.post("/auth/refresh", headers=auth_header)
     assert response.status_code == 200
     assert "token" in response.json()
 
 
-def test_logout(client):
-    correct_user, auth_header = setup_users()
+def test_logout(client, setup_users):
+    correct_user, auth_header = setup_users
 
     response = client.post("/auth/login", json=correct_user)
     assert response.status_code == 200
@@ -70,8 +70,8 @@ def test_logout(client):
     assert len(ip_token_storage) == ips_stored - 1
 
 
-def test_init(client):
-    correct_user, auth_header = setup_users()
+def test_init(client, setup_users):
+    correct_user, auth_header = setup_users
 
     response = client.post("/auth/login", json=correct_user)
     assert response.status_code == 200
