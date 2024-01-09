8b909a815a4c168c351c0cd502e04362572213ce
Adam <a.gepard007@gmail.com>
2024-01-09 19:44:05 +0100

    Added contest summary email

--- backend/emails/email_content.py
+++ backend/emails/email_content.py
@@ -12,3 +12,17 @@ class EmailContentGenerator:
             Fundacja"
         )
         return (subject, body)
+
+    @staticmethod
+    def generate_contest_summary(contest_name, place):
+        subject = f"Wyniki konkursu {contest_name}"
+        body = (
+            f"Szanowni Państwo, \n\
+            dziękujemy za udział w konkursie {contest_name}. \
+            Przesłane przez Państwa prace były fantastyczne. \n\
+            Uzyskany przez państwa wynik: \n\
+            {place} \n\
+            Z poważaniem, \n\
+            Fundacja"
+        )
+        return (subject, body)
