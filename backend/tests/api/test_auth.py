d9d5a435e38fe17d1c974303bbd6a097a7c8376d
misat1505 <misat2002@gmail.com>
2024-01-13 16:21:38 +0100

    redone auth tests

--- backend/tests/api/test_auth.py
+++ backend/tests/api/test_auth.py
@@ -1,6 +1,3 @@
-from backend.api.routers.auth import ip_token_storage
-
-
 def test_login(client, setup_users):
     incorrect_user = {
         "login": "incorrect_login",
@@ -12,12 +9,10 @@ def test_login(client, setup_users):
     response = client.post("/auth/login", json=incorrect_user)
     assert response.status_code == 401
 
-    assert len(ip_token_storage) == 0
-
     response = client.post("/auth/login", json=correct_user)
     assert response.status_code == 200
+    assert "token" in response.cookies
     assert "token" in response.json()
-    assert len(ip_token_storage) == 1
 
 
 def test_refresh(client, setup_users):
@@ -25,6 +20,7 @@ def test_refresh(client, setup_users):
 
     response = client.post("/auth/refresh", headers=auth_header)
     assert response.status_code == 200
+    assert "token" in response.cookies
     assert "token" in response.json()
 
 
@@ -34,24 +30,6 @@ def test_logout(client, setup_users):
     response = client.post("/auth/login", json=correct_user)
     assert response.status_code == 200
 
-    ips_stored = len(ip_token_storage)
-
     response = client.post("auth/logout", headers=auth_header)
-    assert len(ip_token_storage) == ips_stored - 1
-
-
-def test_init(client, setup_users):
-    correct_user, auth_header = setup_users
-
-    response = client.post("/auth/login", json=correct_user)
     assert response.status_code == 200
-
-    response = client.get("/auth/init")
-    assert response.status_code == 200
-    assert "token" in response.json()
-
-    response = client.post("/auth/logout", headers=auth_header)
-    assert response.status_code == 200
-
-    response = client.get("/auth/init")
-    assert response.status_code == 401
+    assert "token" in response.cookies
